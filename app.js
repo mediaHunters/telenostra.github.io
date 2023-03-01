8527dfc2fda809a228f8616cc732e48df58a4213
mediahunters <hello.mediahunters@gmail.com>
2023-03-01 11:14:22 +0100

    update ui

--- app.js
+++ app.js
@@ -1,47 +1,56 @@
-window.addEventListener("load", () => {
-    setTimeout(() => {
-      const loader = document.querySelector(".loader-container");
-      if(loader) {
-        loader.classList.add("hide-loader");
+let opacity = 1;
+let scrollPos = 0;
+
+const textWrapper = document.querySelector(".scroll-text-wrapper");
+const text = document.querySelector(".scroll-text");
+const textBox = document.querySelector(".main-content");
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector("nav");
+
+let bottomLineValue;
+
+
+if (window.innerWidth > 768) {
+  bottomLineValue = textWrapper && (textWrapper.scrollTop + textWrapper.clientHeight) / 4;
+} else {
+bottomLineValue = textWrapper && (textWrapper.scrollTop + textWrapper.clientHeight) / 3;
+
+}
+
+
+window.addEventListener("click", () => {
+  setTimeout(() => {
+    const loader = document.querySelector(".loader-container");
+    loader.classList.add("hide-loader");
+    if (loader) {
       setTimeout(() => {
         loader.remove();
-      }, 2000);
-      }
-    }, 2000);
-  });
-  
-  const menuToggle = document.querySelector(".menu-toggle");
-  const nav = document.querySelector("nav");
-  
-  menuToggle.addEventListener("click", () => {
-    nav.classList.toggle("active");
-  });
-  
-  const textWrapper = document.querySelector(".scroll-text-wrapper");
-  const text = document.querySelector(".scroll-text");
-  const textBox = document.querySelector(".main-content");
-  
-  const bottomLineValue = textWrapper && (textWrapper.scrollTop + textWrapper.clientHeight)  / 3;
-  
-  let opacity = 1;
-  let scrollPos = 0;
-  
-  const checkPosition = () => {
-    const windowY = textBox.scrollTop;
-    if (bottomLineValue > scrollPos && windowY < scrollPos) {
-      text.classList.add("is-visible");
-      text.classList.remove("is-hidden");
-    } else if(bottomLineValue < scrollPos && windowY > scrollPos) {
-      text.classList.add("is-hidden");
-      text.classList.remove("is-visible");
+      }, 1000);
     }
-    scrollPos = windowY;
-  };
-  
-  textBox && textBox.addEventListener("scroll", checkPosition);
-  
+  }, 2000);
+});
 
-  const menuToggle1 = document.querySelector(".menu-toggle");
+
+
+menuToggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+const checkPosition = () => {
+  const windowY = textBox.scrollTop;
+  if (bottomLineValue > scrollPos && windowY < scrollPos) {
+    text.classList.add("is-visible");
+    text.classList.remove("is-hidden");
+  } else if (bottomLineValue < scrollPos && windowY > scrollPos) {
+    text.classList.add("is-hidden");
+    text.classList.remove("is-visible");
+  }
+  scrollPos = windowY;
+};
+
+textBox && textBox.addEventListener("scroll", checkPosition);
+
+const menuToggle1 = document.querySelector(".menu-toggle");
 const menuItems = document.querySelector(".menu-items");
 
 menuToggle1.addEventListener("click", () => {
